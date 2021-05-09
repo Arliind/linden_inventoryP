88a17932a469a5066e91dab4ff4152ba07b952b9
ADRNALN <32118783+Tiimon@users.noreply.github.com>
2021-05-08 20:00:12 -0700

    Fix dealer

--- client/main.lua
+++ client/main.lua
@@ -989,7 +989,7 @@ AddEventHandler('linden_inventory:useItem',function(item)
 end)
 
 local coordinate = {
-    {1656.73, 4.58, 166.12,"Dealer",252.3,0xC99F21C4,"g_m_y_ballasout_01"}
+    {1656.15, 3.19, 166.12,"Dealer",252.3,0xC99F21C4,"a_m_y_business_01"}
 }
 
 Citizen.CreateThread(function()
@@ -1013,42 +1013,4 @@ Citizen.CreateThread(function()
     end
 end)
 
-Citizen.CreateThread(function()
-    while true do
-        local pos = GetEntityCoords(GetPlayerPed(-1), true)
-        Citizen.Wait(0)
-        for _,v in pairs(coordinate) do
-            x = v[1]
-            y = v[2]
-            z = v[3]
-        end
-    end
-end)
-
-
-function DrawText3D(x,y,z, text, scl, font) 
-
-    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
- 
-    local scale = (1/dist)*scl
-    local fov = (1/GetGameplayCamFov())*100
-    local scale = scale*fov
-   
-    if onScreen then
-        SetTextScale(0.0*scale, 1.1*scale)
-        SetTextFont(font)
-        SetTextProportional(1)
-        SetTextColour(255, 255, 255, 255)
-        SetTextDropshadow(0, 0, 0, 0, 255)
-        SetTextEdge(2, 0, 0, 0, 150)
-        SetTextDropShadow()
-        SetTextOutline()
-        SetTextEntry("STRING")
-        SetTextCentre(1)
-        AddTextComponentString(text)
-        DrawText(_x,_y)
-    end
-end
 
