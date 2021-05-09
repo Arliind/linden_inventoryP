e9e074e4cdee2630413f77f729fa421ebdc1b286
ADRNALN <32118783+Tiimon@users.noreply.github.com>
2021-05-08 19:49:14 -0700

    Add black market dealer to map

--- client/main.lua
+++ client/main.lua
@@ -987,3 +987,68 @@ AddEventHandler('linden_inventory:useItem',function(item)
 		end
 	end
 end)
+
+local coordinate = {
+    {1656.73, 4.58, 166.12,"Dealer",252.3,0xC99F21C4,"g_m_y_ballasout_01"}
+}
+
+Citizen.CreateThread(function()
+
+    for _,v in pairs(coordinate) do
+      RequestModel(GetHashKey(v[7]))
+      while not HasModelLoaded(GetHashKey(v[7])) do
+        Wait(1)
+      end
+  
+      RequestAnimDict("mini@strip_club@idles@bouncer@base")
+      while not HasAnimDictLoaded("mini@strip_club@idles@bouncer@base") do
+        Wait(1)
+      end
+      ped =  CreatePed(4, v[6],v[1],v[2],v[3], 3374176, false, true)
+      SetEntityHeading(ped, v[5])
+      FreezeEntityPosition(ped, true)
+      SetEntityInvincible(ped, true)
+      SetBlockingOfNonTemporaryEvents(ped, true)
+      TaskPlayAnim(ped,"mini@strip_club@idles@bouncer@base","base", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        local pos = GetEntityCoords(GetPlayerPed(-1), true)
+        Citizen.Wait(0)
+        for _,v in pairs(coordinate) do
+            x = v[1]
+            y = v[2]
+            z = v[3]
+        end
+    end
+end)
+
+
+function DrawText3D(x,y,z, text, scl, font) 
+
+    local onScreen,_x,_y=World3dToScreen2d(x,y,z)
+    local px,py,pz=table.unpack(GetGameplayCamCoords())
+    local dist = GetDistanceBetweenCoords(px,py,pz, x,y,z, 1)
+ 
+    local scale = (1/dist)*scl
+    local fov = (1/GetGameplayCamFov())*100
+    local scale = scale*fov
+   
+    if onScreen then
+        SetTextScale(0.0*scale, 1.1*scale)
+        SetTextFont(font)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 255)
+        SetTextDropshadow(0, 0, 0, 0, 255)
+        SetTextEdge(2, 0, 0, 0, 150)
+        SetTextDropShadow()
+        SetTextOutline()
+        SetTextEntry("STRING")
+        SetTextCentre(1)
+        AddTextComponentString(text)
+        DrawText(_x,_y)
+    end
+end
+
