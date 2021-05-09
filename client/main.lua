c0700d7335640b2ee83a4b22983bf3a02e5bdb09
Arlind <ahmetajarlind1@gmail.com>
2021-05-09 04:18:03 +0100

    dealer stuff

--- client/main.lua
+++ client/main.lua
@@ -977,3 +977,28 @@ AddEventHandler('linden_inventory:useItem',function(item)
 		end
 	end
 end)
+
+local coordinate = {
+    {1656.15, 3.19, 166.12,"Dealer",252.3,0xC99F21C4,"a_m_y_business_01"}
+}
+
+Citizen.CreateThread(function()
+
+    for _,v in pairs(coordinate) do
+      RequestModel(GetHashKey(v[7]))
+      while not HasModelLoaded(GetHashKey(v[7])) do
+        Wait(1)
+      end
+
+      RequestAnimDict("mini@strip_club@idles@bouncer@base")
+      while not HasAnimDictLoaded("mini@strip_club@idles@bouncer@base") do
+        Wait(1)
+      end
+      ped =  CreatePed(4, v[6],v[1],v[2],v[3], 3374176, false, true)
+      SetEntityHeading(ped, v[5])
+      FreezeEntityPosition(ped, true)
+      SetEntityInvincible(ped, true)
+      SetBlockingOfNonTemporaryEvents(ped, true)
+      TaskPlayAnim(ped,"mini@strip_club@idles@bouncer@base","base", 8.0, 0.0, -1, 1, 0, 0, 0, 0)
+    end
+end)
